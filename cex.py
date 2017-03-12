3c63333e79be9f9ec7a426c4029a2ee6342bd673
Thanh Duc Nguyen <thanh.nguyen-duc@hellopay.com>
2017-03-13 00:51:41 +0700

    update builder

--- cex.py
+++ cex.py
@@ -1,23 +1,37 @@
 import sys
 from subprocess import call
 import click
+import unicodedata
+
+HEADLESS = 'hl'
+
 
 @click.option('-h', default=False, help='run your test with Headless if -h = True')
-@click.option('-t', default=123456, help='run with your tag or list of tags')
-@click.option('-p', default='hp.staging.ph', help='run with your profile')
-
+@click.option('-e', default='staging', help='your test environment')
+@click.option('-c', default='sg', help='your test country')
+@click.option('-t', default='123456,12,1212,1212', help='run with your tag or list of tags')
 
 @click.command()
 
-def cex(t, p):
-    cmd= 'bundle exec' + ' cucumber' + ' -t ' + t + ' -p ' + p
-    print(cmd)
-    call(cmd, shell=True)
 
-def tags_builder
-	'1234'
-def profile_builder(hl = False)
-	'hp.'
+def cex(h,e,c,t):
+    cm = 'bundle exec' + ' cucumber' + ' -p ' + profile_builder(e, c, h) + ' -t ' + tags_builder(t)
+    call(cm, shell=True)
+
+def tags_builder(t):
+	t = t.split(',')
+	for i in xrange(len(t)):
+		t[i] = '@' + t[i]
+	t = ','.join(t)
+	unicodedata.normalize('NFKD', t).encode('ascii','ignore')
+	return t
+
+def profile_builder(e, c, hl = False):
+	if hl is True:
+		p = '.'.join([HEADLESS,e,c])
+	else:
+		p = '.'.join([e,c])
+	return 'hp.' + p
 
 if __name__=='__main__':
     cex()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='test_runner',
+    name='cex',
     version='0.1',
     py_modules=['cex'],
     install_requires=[
